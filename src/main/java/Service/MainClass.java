b27bb141cb7cc8291e8d19161432cb36209c26d5
Sajjad <rabiee.614@gmail.com>
2020-09-27 18:49:48 +0330

    Use JPA for hibernate

--- src/main/java/Service/MainClass.java
+++ src/main/java/Service/MainClass.java
@@ -1,9 +1,16 @@
 package Service;
 
-import Service.Process.Register;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 public class MainClass {
     public static void main(String[] args) {
-        Register.registerProcess();
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("my-persistence-unit");
+        EntityManager em = emf.createEntityManager();
+
+        emf.close();
+        em.close();
     }
 }
