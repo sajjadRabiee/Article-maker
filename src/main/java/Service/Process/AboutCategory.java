ee4f2d713187c4f383f7712e6df44c230f45825d
Sajjad <rabiee.614@gmail.com>
2020-09-28 00:14:23 +0330

    Add show for category Add choose for category

--- src/main/java/Service/Process/AboutCategory.java
+++ src/main/java/Service/Process/AboutCategory.java
@@ -1,4 +1,39 @@
 package Service.Process;
 
+import Repositories.CategoryDAO;
+import Service.Input.InputArea;
+import Service.entities.Category;
+
+import javax.persistence.EntityManager;
+import java.util.Optional;
+
 public class AboutCategory {
+    public static void showAllCategory(EntityManager em){
+        CategoryDAO categoryDAO = new CategoryDAO(em);
+        int i = 0;
+        for(Category cate : categoryDAO.selectAll()){
+            i++;
+            System.out.println("-----------------------------------------------------------");
+            System.out.println(i + ". title : " + cate.getTitle());
+            System.out.println("description : " + cate.getDescription());
+            System.out.println("-----------------------------------------------------------");
+        }
+    }
+
+    public static Category chooseCategory(String s , EntityManager em){
+        CategoryDAO categoryDAO = new CategoryDAO(em);
+        String titleCategory;
+        System.out.println("please enter title of category you want : ");
+        while (true) {
+            titleCategory = InputArea.getName();
+            if (categoryDAO.selectByName(titleCategory).equals(Optional.empty())) {
+                System.out.println("your category is not found \n" +
+                        "you can request your category from admins");
+                continue;
+            } else {
+                break;
+            }
+        }
+        return categoryDAO.selectByName(titleCategory).get();
+    }
 }
